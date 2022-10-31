f790364a452724f78972ac1bc8a9c481251c480c
Taher Quazi <64554080+taherquazi@users.noreply.github.com>
2022-10-31 11:58:46 -0400

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#apt-get remove docker docker-engine docker.io containerd runc
+echo "RUN FROM ROOT or ADD SUDO BEFORE CMD"
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
