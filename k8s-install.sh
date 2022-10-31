b39f446049696c25d38512029b6b4844672a427f
Taher Quazi <64554080+taherquazi@users.noreply.github.com>
2022-10-31 12:00:22 -0400

    Create k8s-install.sh
    This creates kubelet kubeadm kubectl

---
+++ k8s-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+apt-get update -y
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update -y
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
